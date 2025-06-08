4ef8361b46a3f56919e4cbc2f81bddbf23b79fa7
rusiru <evildev360@gmail.com>
2025-06-08 18:26:31 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  // Your other settings go here
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
